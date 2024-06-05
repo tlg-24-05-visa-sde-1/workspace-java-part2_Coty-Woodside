86976ec5889989a9d2df9c1139aa1dc54c1740bd
Coty Woodside <cotyw@tlgcohort.com>
2024-06-05 16:56:43 -0500

    junit introduction before/after/beforeclass/afterclass

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,52 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
 
+    //business object(s) under test - a "fixture"
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("beforeclass");
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("afterclass");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("setup");
+        calc = new Calculator();
+    }
+
+    @After
+    public void cleanUP() throws Exception {
+        System.out.println("cleanup");
+
+    }
+
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
+        System.out.println("testAdd");
         assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+
+    @Test
+    public void testDivide(){
+        System.out.println("testDivide");
+        assertEquals(2.5, calc.divide(5, 2),.001);
+    }
+
+    @Test
+    public void testIsEven(){
+        System.out.println("testIsEven");
+        assertTrue(calc.isEven(10));
     }
 }
