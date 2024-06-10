602b17cb6d6b302317cbe8f821ff41611459f8a5
Coty Woodside <cotyw@tlgcohort.com>
2024-06-10 14:27:24 -0500

    IRSEnum singleton with IRS interface and static factory method

---
+++ Lab05.1/src/gov/irs/IRS.java
@@ -0,0 +1,10 @@
+package gov.irs;
+
+public interface IRS {
+    public void collectTaxes();
+    public void register(TaxPayer payer);
+
+    public static IRS getInstance() {
+        return IRSEnum.INSTANCE;
+    }
+}
