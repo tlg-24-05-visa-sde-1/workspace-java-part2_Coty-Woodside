85acc9a34a0ff90279f8ae0a0d4183b401864462
Coty Woodside <cotyw@tlgcohort.com>
2024-06-12 13:57:06 -0500

    sort via anonymous Comparator classes

--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -19,11 +20,32 @@ class RadishSortTest {
         dump(radishes);
 
         System.out.println("sort by color via comparator class");
-        radishes.sort(new RadishColorComparator());
+        //radishes.sort(new RadishColorComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return r1.getColor().compareTo(r2.getColor());
+            }
+        });
         dump(radishes);
 
         System.out.println("sort by GuysOnTop comparator class");
-        radishes.sort(new RadishGuysOnTopComparator());
+       // radishes.sort(new RadishGuysOnTopComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop());
+            }
+        });
+        dump(radishes);
+
+        System.out.println("sort by tailLength via anonymous class");
+        radishes.sort(  new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        }  );
         dump(radishes);
     }
 
