f610222d98f35c0ff814d0d0c6cf8f945ac18634
Coty Woodside <cotyw@tlgcohort.com>
2024-06-14 10:41:03 -0500

    commit pickle

--- Scratch/src/com/crisp/Pickle.java
+++ Scratch/src/com/crisp/Pickle.java
@@ -1,5 +1,11 @@
 package com.crisp;
 
-class Pickle {
+import org.w3c.dom.ls.LSOutput;
 
+class Pickle {
+    private int sourness;
+
+    public void crunch() {
+        System.out.println("crunching pickle");
+    }
 }
